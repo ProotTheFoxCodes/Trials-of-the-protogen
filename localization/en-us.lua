98338263ac92439b022b2360ce1ea5edb5dfd25f
proto <namespace.dll@gmail.com>
2025-11-24 19:42:35 +0000

    fivemat

--- localization/en-us.lua
+++ localization/en-us.lua
@@ -11,13 +11,16 @@ misc = {
             "All cards start as 2s."
         },
         ch_c_totp_talents3 = {
-            "Whenever a {C:white, X:blue}non King or Queen{} triggers, {C:red}reduce either{} {C:chips}chips{} {C:red} or{} {C:white, X:red}mult{} {C:red}by 10%.{}"
+            "Whenever a {X:blue, C:white}non King or Queen{} triggers, {C:red}reduce either{} {C:chips}chips{} {C:red} or{} {X:red, C:white}mult{} {C:red}by 10%.{}"
         },
         ch_c_totp_talents4 = {
-            "Every 30th card triggered is considered {C:white, X:blue, T:j_totp_unwise_tooltip}Unwise{}."
+            "Every 30th card triggered is considered {X:blue, C:white, T:j_totp_unwise_tooltip}Unwise{}."
         },
         ch_c_totp_talents5 = {
-            "Whenever a card is made {C:white, X:blue, T:j_totp_unwise_tooltip}Unwise{}, it's base chips become the total chip value of the previously declared {C:white, X:blue, T:j_totp_unwise_tooltip}Unwise{} card."
+            "Whenever a card is made {C:white, X:blue, T:j_totp_unwise_tooltip}Unwise{}, it's base chips become"
+        }
+        ch_c_totp_talents6 = {
+            "the total chip value of the previously declared {C:white, X:blue, T:j_totp_unwise_tooltip}Unwise{} card."
         }
     }
 },
