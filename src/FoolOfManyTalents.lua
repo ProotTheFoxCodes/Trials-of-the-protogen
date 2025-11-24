c77d9b48881974e2f5e73bb013884371f9547d5b
proto <namespace.dll@gmail.com>
2025-11-24 21:28:01 +0000

    fool of many talents v1 stable (slight verbump)

--- src/FoolOfManyTalents.lua
+++ src/FoolOfManyTalents.lua
@@ -25,12 +25,16 @@ SMODS.Challenge({
     },
 	restrictions = {
 		banned_cards = {
+			{ id = "v_magic_trick"},
+			{ id = "v_illusion" },
 			{ id = "c_cryptid" },
 			{ id = "c_death" },
             { id = "c_ouija" },
+			{ id = "c_familiar"},
             { id = "j_certificate" },
             { id = "j_marble" },
 			{ id = "j_wee" },
+			{ id = "j_mail" },
             { id = 'p_standard_normal_1', ids = {
                 'p_standard_normal_1', 'p_standard_normal_2',
                 'p_standard_normal_3', 'p_standard_normal_4',
@@ -98,43 +102,45 @@ SMODS.Challenge({
 			{ s = "D", r = "2" },
 			{ s = "D", r = "2" },
 			{ s = "D", r = "2" },
-		},
-		calculate = function(self, context)
-			if context.individual and context.cardarea == G.play and not context.end_of_round then
-				ret = {}
-				G.GAME.totp_talent = G.GAME.totp_talent + 1
-				local talent = G.GAME.totp_talent
-				print(talent)
-				if talent == 1 then
-					G.GAME.totp_talented_nominal = context.other_card:get_chip_bonus()
-				end
-				if talent == 1 or talent % 30 == 0 then
-					totp_new_talented = context.other_card:get_chip_bonus()
-					context.other_card.ability.perma_bonus = 0
-					context.other_card.nominal = G.GAME.totp_talented_nominal
-					G.GAME.totp_talented_nominal = totp_new_talented
-				end
-				ret.extra = {message = totp.talent .. " / 30" }
-				if not context.other_card.base.value == "King" and not context.other_card.base.value == "Queen" then
-					if pseudorandom("shoutouts to gay foxgirls please draw yuri of them", 0, 1) then
-						a = hand_chips * -0.1
-						b = 1
-					else
-						a = 0
-						b = 0.9
-					end
-					ret.chips = a
-					ret.xmult = b
-					ret.remove_default_message = true
-					--[[
-						table.insert(ret, "chips = a")
-						table.insert(ret, "xmult = b")
-						table.insert(ret, "remove_default_message = true")
-					]]
-				end
-				print(ret)
-				return ret
-			end
-    	end
+		}
 	},
+	calculate = function(self, context)
+		if context.individual and context.cardarea == G.play and not context.end_of_round then
+			ret = {}
+			G.GAME.totp_talent = (G.GAME.totp_talent or 0) + 1
+			local talent = G.GAME.totp_talent
+			--print(talent)
+			if talent == 1 then
+				G.GAME.totp_talented_nominal = context.other_card:get_chip_bonus()
+			end
+			if talent == 1 or talent % 30 == 0 then
+				totp_new_talented = context.other_card:get_chip_bonus()
+				context.other_card.ability.perma_bonus = 0
+				context.other_card.nominal = G.GAME.totp_talented_nominal
+				G.GAME.totp_talented_nominal = totp_new_talented
+			end
+			extra = true
+			if context.other_card:get_id() ~= "King" and context.other_card:get_id() ~= "Queen" then
+				if pseudorandom("shoutouts to gay foxgirls please draw yuri of them", 0, 1) then
+					a = hand_chips * -0.1
+					b = 1
+				else
+					a = 0
+					b = 0.9
+				end
+				ret.chips = a
+				ret.xmult = b
+				--[[
+					table.insert(ret, "chips = a")
+					table.insert(ret, "xmult = b")
+					table.insert(ret, "remove_default_message = true")
+				]]
+			end
+			if (extra or 0) then
+				ret.extra = {message = talent % 30 .. " / 30" }
+			end
+			--print(ret)
+			return ret
+		end
+	end
 })
