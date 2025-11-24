aba3674e5e17cf60fb8808c877c5c47a4383c858
proto <namespace.dll@gmail.com>
2025-11-24 19:36:43 +0000

    text size

--- localization/en-us.lua
+++ localization/en-us.lua
@@ -5,16 +5,17 @@ misc = {
     },
     v_text = {
         ch_c_totp_talents1 = {
-            "Start with an {C:attention, T:eternal}Eternal{} {B:blue, C:legendary, T:j_triboulet}Triboulet{}."
+            "Start with an {C:attention, T:eternal}Eternal{} {X:blue, C:legendary, T:j_triboulet}Triboulet{}."
         },
         ch_c_totp_talents2 = {
             "All cards start as 2s."
         },
         ch_c_totp_talents3 = {
-            "Whenever a {B:blue, C:white}non King or Queen{} triggers, {C:red}reduce either{} {C:chips}chips{} {C:red} or {} {B:red, C:white}mult{} {c:red}by 10%.{}"
+            "Whenever a {X:blue, C:white}non King or Queen{} triggers, {C:red}reduce either{} {C:chips}chips{} {C:red} or {} {X:red, C:white}mult{} {C:red}by 10%.{}"
         },
         ch_c_totp_talents4 = {
-            "Every 30th card triggered is considered {B:blue, C:white, T:j_totp_unwise_tooltip}Unwise{}. Whenever a card is made {B:blue, C:white, T:j_totp_unwise_tooltip}Unwise{}, it's base chips become the total chip value of the previously declared {B:blue, C:white, T:j_totp_unwise_tooltip}Unwise{} card."
+            "Every 30th card triggered is considered {X:blue, C:white, T:j_totp_unwise_tooltip}Unwise{}.",
+            "Whenever a card is made {X:blue, C:white, T:j_totp_unwise_tooltip}Unwise{}, it's base chips become the total chip value of the previously declared {X:blue, C:white, T:j_totp_unwise_tooltip}Unwise{} card."
         }
     }
 },
