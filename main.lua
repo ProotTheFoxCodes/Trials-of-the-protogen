1ade0fcdc7098a597771de225ff49484c3090137
Proto :3 <103845094+ProotTheFoxCodes@users.noreply.github.com>
2026-01-02 14:45:05 +0000

    preemptively meet liscense requirements

--- main.lua
+++ main.lua
@@ -1,6 +1,7 @@
 --[[
 Code "inspired" (taken and then modified to fit my needs)
 from ortalab, which was made by eremel
+https://github.com/EremelMods/Ortalab/blob/5a10f033ec29ed72c78bed64e448c18ae1a5ca2f/main.lua
 ]]
 local order = {
     "ModGlobal",
